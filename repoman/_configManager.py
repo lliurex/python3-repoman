3db4d64e6685ee71463d070c24efcb8f7e93ba27
lliurex <juanma1980@gmail.com>
2025-05-09 17:07:35 +0200

    Json as deb822

--- repoman/_configManager.py
+++ repoman/_configManager.py
@@ -2,6 +2,14 @@
 import os,sys,shutil
 import json
 try:
+	from ._repoFile import _repoFile
+except:
+	from _repoFile import _repoFile
+try:
+	from .errorcode import errorEnum
+except:
+	from errorcode import errorEnum
+try:
 	from appconfig import appConfigN4d
 except:
 	appConfigN4d=None
@@ -23,8 +31,11 @@ class _configManager():
 		#content={} 
 		with open(jFile,"r") as f:
 			jcontent=json.loads(f.read())
-		if "Enabled" in jcontent:
-			jcontent.pop("Enabled")
+		cleanFields=["Enabled","changed","disabled_repos"]
+		for cleanf in cleanFields:
+			for repo,repodata in jcontent.items():
+				if cleanf in repodata.keys():
+					repodata.pop(cleanf)
 		#for key in content.keys():
 		#	newkey=key
 		#	if key.endswith("/")==False:
@@ -48,7 +59,7 @@ class _configManager():
 		return(sw)
 
 	def getRepos(self,default=False):
-		sortRepos={}
+		sortrepos={}
 		if os.path.exists(CONFDIR):
 			if default==True:
 				dirs=[os.path.join(CONFDIR,"default")]
@@ -71,12 +82,31 @@ class _configManager():
 				sortkeys=list(repos.keys())
 				sortkeys.sort()
 				for key in sortkeys:
-					sortRepos.update({key:repos[key]})
-					sortRepos[key].update({"Name":key})
-					uris=sortRepos[key].get("repos",[])
+					sortrepos.update({key:repos[key]})
+					sortrepos[key].update({"Name":key})
+					uris=repos[key].get("repos",[])
 					if len(uris)>0:
 						if uris[0].startswith("http://mirror/"):
-							sortRepos[key].update({"available":self._isMirrorEnabled()})
-		return(sortRepos)
+							sortrepos[key].update({"available":self._isMirrorEnabled()})
+		repos=self._getDEB822(sortrepos)
+		return(repos)
 	#def getRepos
+
+	def _getDEB822(self,repos):
+		repos822={}
+		for repokey,repodata in repos.items():
+			repo822=_repoFile()
+			fpath=os.path.join(CONFDIR,"{}.sources".format(repokey[-3]))
+			repo822.setFile(fpath.replace(".sources",".list"))
+			for repo in repodata.get("repos",[]):
+				repo="deb {}\n".format(repo)
+				repo822.raw+=repo
+			fcontent=repo822.getRepoDEB822()
+			repo822.setFile(fpath)
+			repo822.raw=fcontent
+			fcontent.update({"Name":repokey})
+			repos822.update({repokey:fcontent[list(fcontent.keys())[0]]})
+			repos822[repokey].update({"Name":repokey})
+		return(repos822)
+		
 #class _configManager
